afe7f282d44ee51927945b1efd2346a3a734f820
VIGNESH KUMAR S <vigneshkumarselvkumar@gmail.com>
2025-02-06 01:00:10 +0530

    terraform new ec2 creation

---
+++ new_ec2.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "pub_instance_01" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.pubsub.id
+  vpc_security_group_ids      = [aws_security_group.pubsg.id]
+  associate_public_ip_address = true
+  key_name                    = var.key_name
+
+  tags = {
+    Name = "Public EC2 Instance 01"
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -2,5 +2,8 @@ output "public_instance_ip" {
   value = aws_instance.pub_instance.public_ip
 }
 
+output "public_instance_ip_01" {
+  value = aws_instance.pub_instance_01.public_ip
+}
 
 
