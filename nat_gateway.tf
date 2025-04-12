db4992c3eea0a28fe8437076f1250aec4e381a83
VIGNESH KUMAR S <vigneshkumarselvkumar@gmail.com>
2025-04-12 13:45:23 +0530

    Create nat_gateway.tf

---
+++ nat_gateway.tf
@@ -0,0 +1,11 @@
+resource "aws_eip" "nat_eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+  tags = {
+    Name = "NAT Gateway"
+  }
+}
