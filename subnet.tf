b7d4eb7b5c2ebb3459dfc8fc862e81d273c8db4f
VIGNESH KUMAR S <vigneshkumarselvkumar@gmail.com>
2025-04-12 14:15:12 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -11,7 +11,7 @@ resource "aws_subnet" "pubsub" {
 resource "aws_subnet" "private_subnet" {
   vpc_id                  = aws_vpc.myvpc.id
   cidr_block              = var.private_subnet_cidr
-  availability_zone       = "${var.aws_region}a"
+  availability_zone       = "${var.aws_region}c"
   map_public_ip_on_launch = false
 
   tags = {
