e64f8224eec58978e86d3991bbb12649fb344a17
VIGNESH KUMAR S <vigneshkumarselvkumar@gmail.com>
2025-04-12 13:59:42 +0530

    Update security_group.tf

--- security_group.tf
+++ security_group.tf
@@ -15,3 +15,31 @@ resource "aws_security_group" "pubsg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+
+resource "aws_security_group" "private_sg" {
+  name        = "private-ec2-sg"
+  description = "Allow Priavet Access"
+  vpc_id      = aws_vpc.myvpc.id
+
+  
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Private EC2 SG"
+  }
+}
+
