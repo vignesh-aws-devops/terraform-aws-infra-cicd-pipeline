b4073dff1256c9081170b1715b1a6b51b489c7db
VIGNESH KUMAR S <vigneshkumarselvkumar@gmail.com>
2025-04-12 13:51:17 +0530

    Update route_table.tf

--- route_table.tf
+++ route_table.tf
@@ -8,7 +8,7 @@ resource "aws_internet_gateway" "tigw" {
 }
 
 
-resource "aws_route_table" "public-rt" {
+resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.myvpc.id
 
   route {
@@ -23,5 +23,25 @@ resource "aws_route_table" "public-rt" {
 
 resource "aws_route_table_association" "pubsub-associate" {
   subnet_id      = aws_subnet.pubsub.id
-  route_table_id = aws_route_table.public-rt.id
+  route_table_id = aws_route_table.public_rt.id
 }
+
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "Private Route Table"
+  }
+}
+
+resource "aws_route_table_association" "private_rt_assoc" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
