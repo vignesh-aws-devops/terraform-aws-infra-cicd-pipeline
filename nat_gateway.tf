d08772129b8e86921f9136df3fe3422cb3a2eedb
VIGNESH KUMAR S <vigneshkumarselvkumar@gmail.com>
2025-04-12 14:07:55 +0530

    Update nat_gateway.tf

--- nat_gateway.tf
+++ nat_gateway.tf
@@ -4,7 +4,7 @@ resource "aws_eip" "nat_eip" {
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public_subnet.id
+  subnet_id     = aws_subnet.pubsub.id
   tags = {
     Name = "NAT Gateway"
   }
