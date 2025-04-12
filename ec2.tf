76b4de0c009d91559bd4d8570bb60b275b7383e4
VIGNESH KUMAR S <vigneshkumarselvkumar@gmail.com>
2025-04-12 14:17:40 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "pub_instance" {
 
 
 resource "aws_instance" "private_ec2" {
-  ami                         = var.ami
+  ami                         = var.ami_id
   instance_type               = var.instance_type
   subnet_id                   = aws_subnet.private_subnet.id
   vpc_security_group_ids      = [aws_security_group.private_sg.id]
