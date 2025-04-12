9304c527c7769b64f4a19cff58e2fc0453480510
VIGNESH KUMAR S <vigneshkumarselvkumar@gmail.com>
2025-04-12 14:14:42 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -10,3 +10,17 @@ resource "aws_instance" "pub_instance" {
     Name = "Public EC2 Instance 1"
   }
 }
+
+
+resource "aws_instance" "private_ec2" {
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.private_subnet.id
+  vpc_security_group_ids      = [aws_security_group.private_sg.id]
+  associate_public_ip_address = false
+
+  tags = {
+    Name = "Private EC2"
+  }
+}
+
